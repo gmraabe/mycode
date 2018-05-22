57344e43327041f116bc67d59e015e233a1ab3c3
Micah Raabe <gmraabe@gmail.com>
2018-05-22 11:40:06 -0400

    added day2/ipask.py

---
+++ day2/ipask.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# ipask.py
+# Micah Raabe
+
+# Exercise to collect and IP address and MAC address from the user and print them back.
+
+ip_addr = input("Tell me an IP address: ")
+mac_addr = input("Tell me a MAC address: ")
+print("You said your IP address is " + ip_addr + " and your MAC is " + mac_addr)
