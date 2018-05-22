dbd357638057a9cf5e4cd1bc6431492181d171e5
Micah Raabe <gmraabe@gmail.com>
2018-05-22 11:17:16 -0400

    orange.py added as day 2 morning exercise

---
+++ day2/orange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# orange.py
+# Micah Raabe
+
+# This is an opening exercise for Day 2 of class
+# Objective is to use six print() functions to display the word "ORANGE"
+
+print("O", sep="", end="")
+print("R", sep="", end="")
+print("A", sep="", end="")
+print("N", sep="", end="")
+print("G", sep="", end="")
+print("E")
