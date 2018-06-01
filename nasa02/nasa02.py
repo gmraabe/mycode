6dbd6acf0027869e50758d90bb0f77750c78850d
gmraabe <gmraabe@gmail.com>
2018-06-01 11:35:20 -0400

    edit of nasa02

--- nasa02/nasa02.py
+++ nasa02/nasa02.py
@@ -37,22 +37,23 @@ decodeneo = json.loads(neoread.decode('utf-8'))
 #print("\n\nConverted python data")
 #print(decodeneo)
 
-print('Total number of tracked NEO\'s = ' + str(tracked_neo(decodeneo)))
-closest_neo = 0
-closest_neoname = ''
+print('\nTotal number of tracked NEO\'s = ' + str(tracked_neo(decodeneo)))
+closest_neo = 93000000  # initialize variable for the closest distance of a neo (initial value set at 1 AU, or the distance from the earth to the sun)
+closest_neoname = ''  # initialize variable for the name of the closest neo
+
 for neodate in decodeneo['near_earth_objects']:   # loop through the dates
-    x = 0
+    x = 0  # initialize/set variable for the while loop to iteriate through the list
     while x < decodeneo['near_earth_objects'][neodate].__len__():   # loop through range of list
         print('Name = ' + decodeneo['near_earth_objects'][neodate][x]['name'], end='')
         print('  Miles = ' + decodeneo['near_earth_objects'][neodate][x]['close_approach_data'][0]['miss_distance']['miles'], end='')
         print('  Moon Lengths = ' + str(round(moon_len(decodeneo['near_earth_objects'][neodate][x]['close_approach_data'][0]['miss_distance']['miles']), 2)))
-        if closest_neo == 0:
+
+        ## check for and determine the closest neo
+        if float(decodeneo['near_earth_objects'][neodate][x]['close_approach_data'][0]['miss_distance']['miles']) < float(closest_neo):
             closest_neo = decodeneo['near_earth_objects'][neodate][x]['close_approach_data'][0]['miss_distance']['miles']
             closest_neoname = decodeneo['near_earth_objects'][neodate][x]['name']
-        elif decodeneo['near_earth_objects'][neodate][x]['close_approach_data'][0]['miss_distance']['miles'] < closest_neo:
-            closest_neo = decodeneo['near_earth_objects'][neodate][x]['close_approach_data'][0]['miss_distance']['miles']
-            closest_neoname = decodeneo['near_earth_objects'][neodate][x]['name']
-        x = x + 1
+            
+        x = x + 1  # add 1 to the while loop variable to continue to iteriate through the list
 
 print('\nClosest NEO to Earth = ' + closest_neoname)
 print('  With a distance of ' + str(closest_neo) + ' miles and ' + str(round(moon_len(closest_neo), 2)) + ' moon lengths')
